5b995fc33d3dcc6b13de5403bec9d8680dbe0540
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-07-11 23:07:13 +0200

    Init anim segments

--- model/animblock.go
+++ model/animblock.go
@@ -8,16 +8,17 @@ import (
 
 type AnimBlock struct {
 	decoders []Decoder
+	segments []Segment
 }
 
 func newAnimBlock() *AnimBlock {
 	return &AnimBlock{
 		decoders: make([]Decoder, 0),
+		segments: make([]Segment, 0),
 	}
 }
 
 func (block *AnimBlock) initFromDatas(datas *kv3.Kv3Element) error {
-
 	decoders, _ := datas.GetKv3ValueArrayAttribute("m_decoderArray")
 	block.decoders = make([]Decoder, 0, len(decoders))
 
@@ -28,6 +29,18 @@ func (block *AnimBlock) initFromDatas(datas *kv3.Kv3Element) error {
 			return fmt.Errorf("cannot init decoder in AnimBlock.initFromDatas: <%w>", err)
 		}
 		block.decoders = append(block.decoders, dec)
+	}
+
+	segments, _ := datas.GetKv3ValueArrayAttribute("m_segmentArray")
+	block.segments = make([]Segment, 0, len(segments))
+
+	for _, v := range segments {
+		seg := Segment{}
+		err := seg.initFromDatas(v.(*kv3.Kv3Element))
+		if err != nil {
+			return fmt.Errorf("cannot init segment in AnimBlock.initFromDatas: <%w>", err)
+		}
+		block.segments = append(block.segments, seg)
 	}
 
 	return nil
