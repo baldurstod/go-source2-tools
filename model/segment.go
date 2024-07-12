44df5b507e58a8831d19b8089e20e16815c7da19
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-07-12 11:41:47 +0200

    Use a buffer reader to read segment

--- model/segment.go
+++ model/segment.go
@@ -1,7 +1,10 @@
 package model
 
 import (
+	"bytes"
+	"encoding/binary"
 	"errors"
+	"fmt"
 	"log"
 
 	"github.com/baldurstod/go-source2-tools/kv3"
@@ -17,6 +20,7 @@ type Segment struct {
 	boneCount         uint16
 	dataLength        uint16
 	bones             []uint16
+	reader            *bytes.Reader
 }
 
 func (seg *Segment) initFromDatas(datas *kv3.Kv3Element) error {
@@ -35,17 +39,38 @@ func (seg *Segment) initFromDatas(datas *kv3.Kv3Element) error {
 	}
 
 	seg.Container = c
+	seg.reader = bytes.NewReader(c)
 
-	seg.decoderId = uint16(c[0]) + (uint16(c[1]) << 8)
-	seg.bytesPerBone = uint16(c[2]) + (uint16(c[3]) << 8)
-	seg.boneCount = uint16(c[4]) + (uint16(c[5]) << 8)
-	seg.dataLength = uint16(c[6]) + (uint16(c[7]) << 8)
+	/*
+		seg.decoderId = uint16(c[0]) + (uint16(c[1]) << 8)
+		seg.bytesPerBone = uint16(c[2]) + (uint16(c[3]) << 8)
+		seg.boneCount = uint16(c[4]) + (uint16(c[5]) << 8)
+		seg.dataLength = uint16(c[6]) + (uint16(c[7]) << 8)
+	*/
 
-	byteIndex := 8
+	err := binary.Read(seg.reader, binary.LittleEndian, &seg.decoderId)
+	if err != nil {
+		return fmt.Errorf("failed to read segment decoder id: <%w>", err)
+	}
+	binary.Read(seg.reader, binary.LittleEndian, &seg.bytesPerBone)
+	if err != nil {
+		return fmt.Errorf("failed to read segment bytes per bone: <%w>", err)
+	}
+	binary.Read(seg.reader, binary.LittleEndian, &seg.boneCount)
+	if err != nil {
+		return fmt.Errorf("failed to read segment bone count: <%w>", err)
+	}
+	binary.Read(seg.reader, binary.LittleEndian, &seg.dataLength)
+	if err != nil {
+		return fmt.Errorf("failed to read segment data length: <%w>", err)
+	}
+
 	seg.bones = make([]uint16, seg.boneCount)
 	for i := 0; i < int(seg.boneCount); i++ {
-		seg.bones[i] = uint16(c[byteIndex]) + (uint16(c[byteIndex+1]) << 8)
-		byteIndex += 2
+		err := binary.Read(seg.reader, binary.LittleEndian, &seg.bones[i])
+		if err != nil {
+			return fmt.Errorf("failed to read segment bone id: <%w>", err)
+		}
 	}
 
 	log.Println(seg.bones)
