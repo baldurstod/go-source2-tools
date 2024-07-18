66ea20cf45f807fd1123def7b16ea0b58445b9fb
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-07-18 14:18:04 +0200

    Automatically init repositories

--- common_test.go
+++ common_test.go
@@ -7,6 +7,9 @@ import (
 
 const varFolder = "./var/"
 
-func initRepo() {
+func initRepo() bool {
 	repository.AddRepository("dota2", vpk.NewVpkFS("R:\\SteamLibrary\\steamapps\\common\\dota 2 beta\\game\\dota\\pak01_dir.vpk"))
+	return true
 }
+
+var _ = initRepo()
