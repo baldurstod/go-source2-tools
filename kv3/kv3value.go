c73b66dc45d5d635b8266ecf4b2274aa99cf5ba0
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-07-25 14:58:16 +0200

    Added methods Kv3ValueToFloat32(), Kv3ValueToFloat64(), Kv3ValueToBool(), Kv3ValueToString(), Kv3ValueToVector3(), Kv3ValueToQuaternion()

--- kv3/kv3value.go
+++ kv3/kv3value.go
@@ -1,5 +1,7 @@
 package kv3
 
+import "github.com/baldurstod/go-vector"
+
 type Kv3Value interface {
 }
 
@@ -33,6 +35,36 @@ func Kv3ValueToInt64(value Kv3Value) int64 {
 	}
 }
 
+func Kv3ValueToFloat32(value Kv3Value) float32 {
+	switch v := value.(type) {
+	case float32:
+		return v
+	case float64:
+		return float32(v)
+	}
+	return 0
+}
+
+func Kv3ValueToFloat64(value Kv3Value) float64 {
+	switch v := value.(type) {
+	case float32:
+		return float64(v)
+	case float64:
+		return v
+	}
+	return 0
+}
+
+func Kv3ValueToBool(value Kv3Value) bool { /*
+		switch v := value.(type) {
+		case float32:
+			return float64(v)
+		case float64:
+			return v
+		}*/
+	return false
+}
+
 func Kv3ValueToKv3Element(value Kv3Value) *Kv3Element {
 	v, ok := value.(*Kv3Element)
 	if ok {
@@ -40,4 +72,31 @@ func Kv3ValueToKv3Element(value Kv3Value) *Kv3Element {
 	}
 
 	return nil
+}
+
+func Kv3ValueToString(value Kv3Value) string {
+	v, ok := value.(string)
+	if ok {
+		return v
+	}
+
+	return ""
+}
+
+func Kv3ValueToVector3[T float32 | float64](value Kv3Value) vector.Vector3[T] {
+	a, ok := value.([]T)
+	if ok && len(a) == 3 {
+		return vector.Vector3[T]{a[0], a[1], a[2]}
+	}
+
+	return vector.Vector3[T]{}
+}
+
+func Kv3ValueToQuaternion[T float32 | float64](value Kv3Value) vector.Quaternion[T] {
+	a, ok := value.([]T)
+	if ok && len(a) == 4 {
+		return vector.Quaternion[T]{a[0], a[1], a[2], a[3]}
+	}
+
+	return vector.Quaternion[T]{}
 }
