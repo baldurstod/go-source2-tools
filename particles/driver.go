58ce31355cf101b17575f4d27151b1245cdbce2c
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-08-09 14:23:01 +0200

    Driver: added default values for AttachType, EntityName

--- particles/driver.go
+++ particles/driver.go
@@ -10,9 +10,14 @@ type Driver struct {
 }
 
 func (driver *Driver) initFromDatas(datas *kv3.Kv3Element) error {
+	var ok bool
 	driver.ControlPoint, _ = datas.GetIntAttribute("m_iControlPoint")
-	driver.AttachType, _ = datas.GetStringAttribute("m_iAttachType")
+	if driver.AttachType, ok = datas.GetStringAttribute("m_iAttachType"); !ok {
+		driver.AttachType = "PATTACH_ABSORIGIN_FOLLOW"
+	}
 	driver.AttachmentName, _ = datas.GetStringAttribute("m_attachmentName")
-	driver.EntityName, _ = datas.GetStringAttribute("m_entityName")
+	if driver.EntityName, ok = datas.GetStringAttribute("m_entityName"); !ok {
+		driver.EntityName = "self"
+	}
 	return nil
 }
